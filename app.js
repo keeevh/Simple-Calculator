bd1681d0a8ad509bc023fc893c2ac7b89d6ce880
Kevin Henrich <kevinhenrich@icloud.com>
2022-08-31 16:42:41 +0200

    stored all buttons and both number display fields in variables

---
+++ app.js
@@ -0,0 +1,6 @@
+const nbrBtn = document.querySelectorAll("nbrButton");
+const calcBtn = document.querySelectorAll(".calcBtn");
+const clearBtn = document.querySelector(".resetBtn");
+const eqlBtn = document.querySelector(".eqlBtn");
+const upperDisplay = document.querySelector(".input1");
+const lowerDisplay = document.querySelector(".input2");
