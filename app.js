8578eeb2ffb8796e5b6a6036a438b352c6bef6c8
Kevin Henrich <kevinhenrich@icloud.com>
2022-08-31 17:59:06 +0200

    Added Event Listener to number buttons

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const nbrBtn = document.querySelectorAll("nbrButton");
+const numBtn = document.querySelectorAll(".numBtn");
 const calcBtn = document.querySelectorAll(".calcBtn");
 const clearBtn = document.querySelector(".resetBtn");
 const eqlBtn = document.querySelector(".eqlBtn");
 const upperDisplay = document.querySelector(".input1");
 const lowerDisplay = document.querySelector(".input2");
+
+// Adding numbers to lowerDisplay on Number Button click
+numBtn.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    let button = e.target.innerText;
+    lowerDisplay.value += button;
+  });
+});
